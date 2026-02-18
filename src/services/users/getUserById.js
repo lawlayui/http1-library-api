e504b6144171266c594ea91d4eaf70ccbdf9c1c1
lawlayui <lawlayui@gmail.com>
2026-02-18 12:34:08 +0700

    fixed an error where the user got the password

--- src/services/users/getUserById.js
+++ src/services/users/getUserById.js
@@ -2,6 +2,6 @@ const { pool } = require('../../core/getConnection');
 
 
 module.exports = async (user_id) => {
-    const [result] = await pool.query('select * from users where id = ?', [user_id]);
+    const [result] = await pool.query('select id, username, role from users where id = ?', [user_id]);
     return result[0];
 };  
