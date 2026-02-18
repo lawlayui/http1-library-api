93527eb096a4515a14362f1fb2a9553bf89d7930
lawlayui <lawlayui@gmail.com>
2026-02-18 12:29:27 +0700

    only HOST is checked in the content-type header

--- src/middleware/jsonMiddleware.js
+++ src/middleware/jsonMiddleware.js
@@ -1,15 +1,16 @@
 
 
 module.exports = async (req, res) => {
-    const headers = req.headers;
-    const header = 'application/json';
-    if (headers['content-type'] === header) {
-        return;
+    if (req.method === "POST") {
+        const headers = req.headers;
+        const header = 'application/json';
+        if (headers['content-type'] === header) {
+            return;
+        }
+        res.writeHead(415, {'content-type': 'application/json'});
+        res.end(JSON.stringify({
+            status: 'error',
+            message: 'unsupported media type'
+        }));
     }
-    res.writeHead(415, {'content-type': 'application/json'});
-    res.end(JSON.stringify({
-        status: 'error',
-        message: 'unsupported media type'
-    }));
-    return;
 };
